8691420d423b4dacbe7c06483e2b1407b16f7fd4
Marck Carrion Guzman <mcarrion@kcramsolutions.com>
2025-03-13 08:28:12 +0100

    refactor(service/user): changes repositories types
    We pass to use a trait instead of structure

--- services/user/src/domain/repositories/user_repository.rs
+++ services/user/src/domain/repositories/user_repository.rs
@@ -1,4 +1,10 @@
+use errors::database::data::query::QueryError;
+use uuid::Uuid;
 
+use crate::domain::user_entity::UserEntity;
 
-pub struct UserRepository {
+pub trait UserRepository {
+    fn find_one_by_id(&mut self, user_id: Uuid) -> Result<UserEntity, QueryError>;
+    fn list(&mut self, page: i32, limit: i32) -> Result<Vec<UserEntity>, QueryError>;
+    fn create(&mut self, user: UserEntity) -> Result<bool, QueryError>;
 }
